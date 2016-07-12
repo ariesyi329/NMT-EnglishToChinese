3ac9c2dea8990891a3ae921a119a2531a6bc170c
Yi Liu <yi.liu.nyu@gmail.com>
2016-07-12 13:30:31 -0400

    translate

---
+++ results/attention_nmt_no_seg/translate_multi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=60:00:00
+#PBS -l mem=4GB
+#PBS -N translate.seg.att
+#PBS -j oe
+
+## on hpc
+#module purge
+#module load theano/0.7.0
+#cd ./NLUDR/NMT-EnglishToChinese/
+
+#THEANO_FLAGS=floatX=float32 python -u -m code.model.attention_nmt.simple_translater -n \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/model.npz \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.train.seg.tok.pkl \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/data/zh/seg/zh.test.seg.tok \
+    #/home/ql516/NLUDR/NMT-EnglishToChinese/results/attention_nmt_seg/test.translation 
+
+
+
+# on laptop
+cd ../..
+THEANO_FLAGS=floatX=float32 python -u -m code.model.attention_nmt.multi_simple_translater -n \
+    $HOME/Documents/Data\ Science/summer_2016/NMT/NMT-EnglishToChinese/code/experiments/attention_nmt_no_seg/model_multi.npz \
+    $HOME/Documents/Data\ Science/summer_2016/NMT/NMT-EnglishToChinese/data/zh/no_seg/zh.train.no_seg.tok.pkl \
+    $HOME/Documents/Data\ Science/summer_2016/NMT/NMT-EnglishToChinese/data/en/train.en.tok.pkl \
+    $HOME/Documents/Data\ Science/summer_2016/NMT/NMT-EnglishToChinese/data/zh/no_seg/zh.test.no_seg.tok \
+    $HOME/Documents/Data\ Science/summer_2016/NMT/NMT-EnglishToChinese/results/attention_nmt_no_seg/test.translation.no_seg.multi
